95bd98a1215506b44e29d9ac0c6a3b513e886d5e
Kaio <kaioaguanais@gmail.com>
2020-11-19 11:15:11 -0800

    Initial Commit

---
+++ AppleStoreTracking/AppleStoreTrackingApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleStoreTrackingApp.swift
+//  AppleStoreTracking
+//
+//  Created by Kaio Guanais on 2020-11-19.
+//
+
+import SwiftUI
+
+@main
+struct AppleStoreTrackingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
